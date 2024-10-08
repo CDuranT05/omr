8f01b6fde96e7e20e1ff47281fccadf8179e566a
CDuranT05 <claudiadurantupano@gmail.com>
2024-10-08 12:36:39 -0400

    verificado

---
+++ NetMvc/Models/Db/Countrie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace NetMvc.Models.Db
+{
+    public class Countrie
+    {
+        [Key]
+        public int Code { get; set; }
+
+        [StringLength(45)]
+        public string Name { get; set; }
+
+        [StringLength(45)]
+        public string ContinentName { get; set; }
+    }
+}
---
+++ NetMvc/Models/Db/users.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetMvc.Models.Db
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [StringLength(1)]
+        public string Gender { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int CountryCode { get; set; }
+
+        public DateTime Create_at { get; set; }
+
+    }
+}
